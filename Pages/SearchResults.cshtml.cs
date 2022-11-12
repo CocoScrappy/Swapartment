ba67e2c1accc703232302d8726b592b252ac843e
Alina Gotcherian <alina.gotcherian@gmail.com>
2022-11-12 18:25:09 -0500

    some small fixes - pushing before I start something new

--- Pages/SearchResults.cshtml.cs
+++ Pages/SearchResults.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Swapartment.Models;
 
-namespace Swapartment.Pages
+namespace Swapartment.Pages;
+
+public class SearchResultsModel : PageModel
 {
-    public class SearchResultsModel : PageModel
+  private readonly Swapartment.Areas.Identity.Data.SwapartmentIdentityDbContext _context;
+
+  public SearchResultsModel(Swapartment.Areas.Identity.Data.SwapartmentIdentityDbContext context)
+  {
+    _context = context;
+  }
+  public IList<Property> Property { get; set; } = default!;
+
+  public async Task OnGetAsync()
+  {
+    if (_context.Properties != null)
     {
-        public void OnGet()
-        {
-        }
+      Property = await _context.Properties.ToListAsync();
     }
+  }
 }
