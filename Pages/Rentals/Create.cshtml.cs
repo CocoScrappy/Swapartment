171f4a3f541adac6607048cb9c4a702df950281c
Louis <louis.dumasv@gmail.com>
2022-11-14 09:13:19 -0500

    carouselNotFixed

--- Pages/Rentals/Create.cshtml.cs
+++ Pages/Rentals/Create.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,6 +13,7 @@ using Swapartment.Models;
 
 namespace Swapartment.Pages_Rentals
 {
+  [Authorize]
   public class CreateModel : PageModel
   {
     private readonly SwapartmentIdentityDbContext _context;
